5814dc7bcb84eb90f373239faf5ec8ba67b2e0bc
Marcio Junior <marciojunior_eu@yahoo.com.br>
2016-07-13 15:22:44 -0300

    Random jasmine tests on protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,3 +1,7 @@
 exports.config = {
-  specs: ['client/**/*.e2e.spec.js']
+  specs: ['client/**/*.e2e.spec.js'],
+  onPrepare() {
+    jasmine.getEnv().randomizeTests(true);
+    jasmine.getEnv().seed(process.env.SEED);
+  }
 };
